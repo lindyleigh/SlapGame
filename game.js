27e7d19a1ceb2649e4bc5cfccf3e79a478fc1d7e
Lindy Crain <lindylcrain@gmail.com>
2016-06-23 19:23:22 -0600

    Add files via upload

---
+++ game.js
@@ -0,0 +1,10 @@
+var choice = confirm("Congratulations! You have discovered the magical land of Unitopia! Your job is to help Glitter the Unicorn retrieve her magic powers back from the Evil Wizard. It won't be easy, but with your help, we can do it! There may even be a reward at the end! Do you want to play?")
+
+if (choice) {
+    alert("Let's do this!")
+    
+} 
+
+else {
+    alert("Oh no! Come back when you are ready to fight!")
+}
