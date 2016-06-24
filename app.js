d4f2a3a5bf8b10bd89e49a732c12a8bd59b71336
Lindy Crain <lindylcrain@gmail.com>
2016-06-23 19:55:21 -0600

    changes

--- app.js
+++ app.js
@@ -9,22 +9,5 @@ else {
     alert("Oh no! Come back when you are ready to fight!")
 }
 
-var health = 100;
 
-function update(attackType){
-  if (health <=0) {
-  console.log("The Evil Wizard is dead! Glitter regained her powers!");
-  alert("The Evil Wizard is dead! Glitter regained her powers!");
-  } else {
-    var comment = "You " + attackType + "ed the Evil Wizard!" 
-    console.log("Continue fighting player.")
-  document.getElementById('health').innerText = health;
-  document.getElementById('comment').innerText = comment;
-  }
-}
-
-function damageplayer(amount, attackType) {
-  health = health - amount;
-  update(attackType)
-}
 
